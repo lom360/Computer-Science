a4ca0803c23b1503837f9d32337bd78144707178
lom360 <lom360@mail.fresnostate.edu>
2017-10-03 23:53:16 -0700

    Added loop_reading.py
    The purpose of this assignment is to read the codes involving loops and
    predict the output of each code. I have only added the programs code. At
    a later time I will add the answer in comments.

---
+++ python3.5/Week1-Python Basics/loop_reading.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 23:48:55 2017
+
+@author: Lom
+"""
+
+"""Problem 1"""
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
+
+"""Problem 2"""
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
+
+"""Problem 3"""
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
+
+"""Problem 4"""
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
+
+"""Problem 5"""
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
