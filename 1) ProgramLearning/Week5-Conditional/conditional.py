b265333f7439c7c49b7529f1cbd2791544258da8
lom360 <lom360@mail.fresnostate.edu>
2018-12-27 01:22:41 -0800

    Added try and except statement in code for conditional.py

--- 1) ProgramLearning/Week5-Conditional/conditional.py
+++ 1) ProgramLearning/Week5-Conditional/conditional.py
@@ -1,9 +1,17 @@
 hrs = input("Enter Hours: ")
-h = float(hrs)
 rate = input("Enter Pay Rate: ")
-r = float(rate)
+# Coverting string input to int.
+try :
+    h = float(hrs)
+    r = float(rate)
+except :
+    print("There has been an issue. Quiting program.")
+    quit()
 
+# If worked over 40 hours will calculate overtime pay.
 if h > 40 :
+    # Up to 40hrs will be paid normal rate
+    # Extra hours will increase by 1.5
     overtime_rate = r * 1.5
     overtime_hours = h - 40
     gross_pay = (40 * r) + (overtime_hours * overtime_rate)
