f8d39508997d248fdbc369c2fb859c4325c36c67
lom360 <lom360@mail.fresnostate.edu>
2018-12-29 00:40:44 -0800

    Added words into a list from .txt eliminated duplicates.

--- 2) DataStructures/Week4-Lists/split.py
+++ 2) DataStructures/Week4-Lists/split.py
@@ -1,6 +1,16 @@
 fname = input("Enter file name: ")
 fh = open(fname)
 
-lst = list(fh)
-for line in lst:
-    print(line.rstrip())
+lst = list()
+
+for line in fh:
+    line = line.rstrip() # Eliminates newline
+    words = line.split() # Split individual words into an array.
+    for word in words :  # Nested loop to check for word duplicates.
+        if word in lst :
+            continue
+        else :
+            lst.append(word)
+
+lst.sort()
+print(lst)
