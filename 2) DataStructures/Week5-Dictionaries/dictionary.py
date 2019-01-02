9520f53ea29243c0bc9e7924cb01982093f74fb7
lom360 <lom360@mail.fresnostate.edu>
2019-01-01 22:43:25 -0800

    Completed dictionary assignment

--- 2) DataStructures/Week5-Dictionaries/dictionary.py
+++ 2) DataStructures/Week5-Dictionaries/dictionary.py
@@ -3,19 +3,19 @@ if fname != "mbox-short.txt": fname = "mbox-short.txt"
 fhandle = open(fname)
 
 email = dict()
+address = ""
 max = 0
 
 for line in fhandle :
     line = line.rstrip()
-    if "From" in line :
+    # Second condintion checks how much words are in the line.
+    # The line we are looking for has a specific format that must consiste of 7 words
+    if "From" in line and len(line.split()) == 7:
         words = line.split()
-    else :
-        continue
-    email[words[1]] = email.get(words[1], 0) + 1
-    print(email.values())
-    # for word in words :
-    #     if email[word] > max :
-    #         max = email[word]
+        email[words[1]] = email.get(words[1], 0) + 1
+        for mail in email :
+            if max < email[mail] :
+                address = mail
+                max = email[mail]
 
-print(email.keys())
-print(max)
+print(address, max)
