88e09b7e663a256ebb6368ec5c188f6f3021cb1d
lom360 <lom360@mail.fresnostate.edu>
2019-01-02 01:41:24 -0800

    Improved assignment

--- 2) DataStructures/Week6-Tuples/tuple.py
+++ 2) DataStructures/Week6-Tuples/tuple.py
@@ -4,13 +4,13 @@ if fname != "mbox-short.txt" : fname = "mbox-short.txt"
 fhandle = open(fname)
 
 hour = dict()
+lst = list()
 
 for line in fhandle : 
     line = line.rstrip()
     words = line.split()
     if "From" in words and len(words) == 7 :
         word = words[5]
-        numbers = word.split(':')
-        for key in numbers :
-            hour[key[0]] = hour.get(key[0], 0) + 1
-print(hour.items())
+        number = word.split(':')
+        hour[number[0]] = hour.get(number[0], 0) + 1
+            
