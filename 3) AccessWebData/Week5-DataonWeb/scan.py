7ad99e588b8e95079e68621fc930a1d592d6a88f
lom360 <lom360@mail.fresnostate.edu>
2019-01-08 03:30:51 -0800

    Editing code

--- 3) AccessWebData/Week5-DataonWeb/scan.py
+++ 3) AccessWebData/Week5-DataonWeb/scan.py
@@ -4,6 +4,9 @@ import urllib.error
 from xml.etree import ElementTree as ET
 import ssl
 
+def parse_website() :
+    print("Hello World")
+
 api_key = False
 # If you have a Google Places API key, enter it here
 # api_key = 'AIzaSy___IDByT70'
@@ -20,28 +23,38 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+comments = dict()
+
 while True:
-    address = input('Enter location: ')
-    if len(address) < 1:
+    url = input('Enter url: ')
+    if len(url) < 1:
         break
+    
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False:
-        parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
 
-    data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
-    tree = ET.fromstring(data)
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
+# while True:
+#     address = input('Enter location: ')
+#     if len(address) < 1:
+#         break
 
-    print('lat', lat, 'lng', lng)
-    print(location)
+#     parms = dict()
+#     parms['address'] = address
+#     if api_key is not False:
+#         parms['key'] = api_key
+#     url = serviceurl + urllib.parse.urlencode(parms)
+#     print('Retrieving', url)
+#     uh = urllib.request.urlopen(url, context=ctx)
+
+#     data = uh.read()
+#     print('Retrieved', len(data), 'characters')
+#     print(data.decode())
+#     tree = ET.fromstring(data)
+
+#     results = tree.findall('result')
+#     lat = results[0].find('geometry').find('location').find('lat').text
+#     lng = results[0].find('geometry').find('location').find('lng').text
+#     location = results[0].find('formatted_address').text
+
+#     print('lat', lat, 'lng', lng)
+#     print(location)
