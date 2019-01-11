7fc9eacdf81d069dac0f9067fd2817aaa947f0df
lom360 <lom360@mail.fresnostate.edu>
2019-01-11 01:53:08 -0800

    Completed 1 assignment for week 6

--- 3) AccessWebData/Week6-JsonAndRest/json1.py
+++ 3) AccessWebData/Week6-JsonAndRest/json1.py
@@ -1,21 +1,29 @@
+import urllib.request, urllib.parse, urllib.error
 import json
 
-data = '''
-[
-  { "id" : "001",
-    "x" : "2",
-    "name" : "Chuck"
-  } ,
-  { "id" : "009",
-    "x" : "7",
-    "name" : "Brent"
-  }
-]'''
+def char_count(file) :
+    temp = json.dumps(file)
+    print("Retrieved", len(temp), " characters")
 
-info = json.loads(data)
-print('User count:', len(info))
+# read() turns file into a string.
+# decode() wile turn the file into an object
 
-for item in info:
-    print('Name', item['name'])
-    print('Id', item['id'])
-    print('Attribute', item['x'])
+url = input("Enter location: ")
+json_url = urllib.request.urlopen(url)
+
+# Storing the object gained from the url and store in data as a dictionary.
+print("Retrieving", url)
+data = json.loads(json_url.read().decode())
+comments = data["comments"]
+
+# Counts number of characters in file.
+char_count(data)
+
+total = 0
+count = 0
+for comment in comments :
+    total = total + comment['count']
+    count += 1
+
+print("Count:", count)
+print("Sum:", total)
